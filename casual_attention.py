23f1659bb95c728081b7cae8f417a71e274d46db
BriceLucifer <2376671337@qq.com>
2025-09-09 19:26:00 +0800

    use the new mask function in pytorch

--- casual_attention.py
+++ casual_attention.py
@@ -29,11 +29,37 @@ print(atten_weights)
 # 上下文长度
 context_length = atten_weights.shape[0]
 # 设置掩码为1 长度为上下文长度 对角线为1
-mask_simple = torch.tril(torch.ones(context_length, context_length))
-masked_simple = mask_simple * atten_weights
-print(masked_simple)
+# mask_simple = torch.tril(torch.ones(context_length, context_length))
+# masked_simple = mask_simple * atten_weights
+# print(masked_simple)
 
-# 归一化
-row_sums = masked_simple.sum(dim=-1, keepdim=True)
-masked_simple_norm = masked_simple / row_sums
-print(masked_simple_norm)
+# # 归一化
+# row_sums = masked_simple.sum(dim=-1, keepdim=True)
+# masked_simple_norm = masked_simple / row_sums
+# print(masked_simple_norm)
+# 被注释的代码虽然能够完成工作 但是不足够更高的效率去计算
+
+# 多了一步掩码分析
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+'''
+    [0,1,1,1]
+    [0,0,1,1]
+    [0,0,0,1]
+    [0,0,0,0]
+'''
+masked = atten_scores.masked_fill(mask=mask.bool(), value=-torch.inf)
+'''
+mask.bool():
+    [false, true, true, true]
+    [false, false, true, true]
+    [false, false, false, true]
+    [false, false, false, false]
+
+replace true with -inf or false with the true value
+'''
+print(masked)
+
+# normalization
+atten_weights = torch.softmax(masked / keys.shape[-1] ** 0.5 , dim=-1)
+print(atten_weights) # same result with previous operation with more efficient computation
+
